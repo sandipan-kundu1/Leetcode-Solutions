3ea76d8d975b27357cfa42a0b409e5d8b546eb62
Sandipan Kundu <158579989+sandipan-kundu1@users.noreply.github.com>
2024-05-25 16:14:03 +0530

    Time: 11 ms (82.33%), Space: 19.3 MB (57.96%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,14 +1,39 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int xorr=0;
-        for(int i=0;i<nums.size();i++){
-            xorr=xorr^nums[i];
+        int xorr = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            xorr = xorr ^ nums[i];
         }
         return xorr;
-        // map<long ,int> mpp;
-        // for(int i=0;i<nums.size();i++){
-
+        // map<int, int> mpp;
+        // for(int i = 0; i < nums.size(); i++) {
+        //     mpp[nums[i]]++;
         // }
+        // for(auto it : mpp) {
+        //     if(it.second == 1)
+        //         return it.first;
+        // }
+        // return -1;
+        // int maxi = nums[0];
+        // int n = nums.size();
+
+        // for (int i = 1; i < n; i++) {
+        //     maxi = max(maxi, nums[i]);
+        // }
+
+        // vector<int> hash(maxi + 1, 0);
+
+        // for (int i = 0; i < n; i++) {
+        //     hash[nums[i]]++;
+        // }
+
+        // for (int i = 0; i < n; i++) {
+        //     if (hash[nums[i]] == 1) {
+        //         return nums[i];
+        //     }
+        // }
+
+        // return -1;
     }
 };
