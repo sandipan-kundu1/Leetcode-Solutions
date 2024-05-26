87c7ad6f77cca9d0cf41715c925c614f13ae600f
Sandipan Kundu <158579989+sandipan-kundu1@users.noreply.github.com>
2024-05-26 20:01:48 +0530

    Time: 11 ms (63.97%) | Memory: 13.6 MB (44.77%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,23 +1,18 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        //Hashmap approach(better)   for brute force nested loop case
+        // Hashmap approach(better)   for brute force nested loop case
         map<int, int> mpp;
-        int i=0;
-        // vector<int> ans;
-        while(i<nums.size()){
-            int more=target-nums[i];
-            
-            if(mpp.find(more)!=mpp.end())
-            {
-                // ans.push_back(mpp[more]);
-                // ans.push_back(i);
-                return {mpp[more],i};
+        for (int i = 0; i < nums.size(); i++) {
+            int more = target - nums[i];
+
+            if (mpp.find(more) != mpp.end()) {
+
+                return {mpp[more], i};
             }
-            mpp[nums[i]]=i;
-            i++;
+            mpp[nums[i]] = i;
         }
         // return ans;
-        return {-1,-1};
+        return {-1, -1};
     }
 };
