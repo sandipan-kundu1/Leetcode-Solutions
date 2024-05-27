464084393288c8c02ad499a064c024fbe835980f
Sandipan Kundu <158579989+sandipan-kundu1@users.noreply.github.com>
2024-05-28 01:09:28 +0530

    Time: 128 ms (53.63%) | Memory: 74 MB (48.40%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -1,22 +1,39 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        int lastsmaller=INT_MIN;
-        int cnt=0,longest=0;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]-1==lastsmaller)
-            {
-                cnt+=1;
-                lastsmaller=nums[i];
+        unordered_set<int> st;
+        int cnt,longest=0;
+        for(auto i:nums)
+        st.insert(i);
+        for(auto it:st){
+            if(st.find(it-1)==st.end()){
+                int cnt=0;
+                int x=it;
+                while(st.find(x)!=st.end()){
+                    x++;
+                    cnt++;
+                }
+                longest=max(longest,cnt);
             }
-            else if(nums[i]!=lastsmaller){
-                
-                cnt=1;
-                lastsmaller=nums[i];
-            }
-            longest=max(longest,cnt);
+            
         }
         return longest;
+        // sort(nums.begin(),nums.end());
+        // int lastsmaller=INT_MIN;
+        // int cnt=0,longest=0;
+        // for(int i=0;i<nums.size();i++){
+        //     if(nums[i]-1==lastsmaller)
+        //     {
+        //         cnt+=1;
+        //         lastsmaller=nums[i];
+        //     }
+        //     else if(nums[i]!=lastsmaller){
+                
+        //         cnt=1;
+        //         lastsmaller=nums[i];
+        //     }
+        //     longest=max(longest,cnt);
+        // }
+        // return longest;
     }
 };
