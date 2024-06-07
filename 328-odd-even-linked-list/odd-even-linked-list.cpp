1ccc60611c25e9a78ef3bab1c65ce3c50ae54b94
Sandipan Kundu <158579989+sandipan-kundu1@users.noreply.github.com>
2024-06-08 01:10:08 +0530

    Time: 4 ms (90.15%) | Memory: 14.3 MB (6.46%) - LeetSync

--- 328-odd-even-linked-list/odd-even-linked-list.cpp
+++ 328-odd-even-linked-list/odd-even-linked-list.cpp
@@ -13,17 +13,40 @@ public:
     ListNode* oddEvenList(ListNode* head) {
         if(head==NULL || head->next==NULL)
         return head;
-        ListNode* odd=head;
-        ListNode* even=head->next;
-        ListNode* evenhead=even;
-        while(even && even->next){
-            odd->next= odd->next->next;
-            even->next= even->next->next;
-            odd=odd->next;
-            even=even->next;
+        // ListNode* odd=head;
+        // ListNode* even=head->next;
+        // ListNode* evenhead=even;
+        // while(even && even->next){
+        //     odd->next= odd->next->next;
+        //     even->next= even->next->next;
+        //     odd=odd->next;
+        //     even=even->next;
 
+        // }
+        // odd->next= evenhead;
+
+        vector<int> arr;
+        ListNode* temp=head;
+        while(temp && temp->next){
+            arr.push_back(temp->val);
+            temp=temp->next->next;
         }
-        odd->next= evenhead;
+        if(temp)
+        arr.push_back(temp->val);
+        temp=head->next;
+        while(temp && temp->next){
+            arr.push_back(temp->val);
+            temp=temp->next->next;
+        }
+        if(temp)
+        arr.push_back(temp->val);
+        temp=head;
+        int i=0;
+        while(temp && i<arr.size()){
+            temp->val=arr[i];
+            i++;
+            temp=temp->next;
+        }
         return head;
     }
 };
