41a274d84d99d19a84184576cfc74a6ba2a7c79f
Sandipan Kundu <158579989+sandipan-kundu1@users.noreply.github.com>
2024-06-12 17:03:45 +0530

    Time: 144 ms (28.82%) | Memory: 73.5 MB (20.27%) - LeetSync

--- 148-sort-list/sort-list.cpp
+++ 148-sort-list/sort-list.cpp
@@ -11,19 +11,67 @@
 class Solution {
 public:
     ListNode* sortList(ListNode* head) {
-        ListNode *temp=head;
-        multiset<int> st;
-        while(temp){
-            st.insert(temp->val);
-            temp=temp->next;
-        }
+        // ListNode *temp=head;
+        // multiset<int> st;
+        // while(temp){
+        //     st.insert(temp->val);
+        //     temp=temp->next;
+        // }
         
-        temp=head;
-        for(auto i=st.begin();i!=st.end();i++){
-            temp->val= *i;
-            st.erase(i);
-            temp=temp->next;
-        }
+        // temp=head;
+        // for(auto i=st.begin();i!=st.end();i++){
+        //     temp->val= *i;
+        //     st.erase(i);
+        //     temp=temp->next;
+        // }
+        // return head;
+        if(head==NULL || head->next ==NULL)
         return head;
+        
+        ListNode *mid= findmid(head);
+        ListNode* left=head,*right=mid->next;
+        mid->next=nullptr;
+        left=sortList(left);
+        right=sortList(right);
+        return merge2(left,right);
+    }
+
+    public: 
+    ListNode* merge2(ListNode *head1,ListNode *head2){
+        ListNode* temp1=head1,*temp2=head2;
+        ListNode* dummy=new ListNode(-1);
+        ListNode *temp=dummy;
+        while(temp1!=NULL && temp2!=NULL){
+            if(temp1->val<temp2->val){
+                temp->next=temp1;
+                temp=temp1;
+                temp1=temp1->next;
+            }
+            else{
+                temp->next=temp2;
+                temp=temp2;
+                temp2=temp2->next;
+            }
+        }
+        if(temp1){
+            temp->next=temp1;
+            
+        }
+        else if(temp2){
+            temp->next=temp2;
+            
+        }
+        return dummy->next;
+    }
+    public:
+    ListNode *findmid(ListNode* head){
+        ListNode* slow=head;
+        ListNode* fast=head->next;
+        while(fast!=NULL && fast->next!=NULL){
+            slow=slow->next;
+            fast=fast->next->next;
+
+        }
+        return slow;
     }
 };
