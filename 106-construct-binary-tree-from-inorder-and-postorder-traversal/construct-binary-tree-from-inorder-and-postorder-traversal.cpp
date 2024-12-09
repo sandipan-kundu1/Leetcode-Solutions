e3359cb30db52f843c51eac8646ee3c874816dcc
Sandipan Kundu <158579989+sandipan-kundu1@users.noreply.github.com>
2024-12-10 02:03:27 +0530

    Time: 0 ms (100.00%) | Memory: 27.6 MB (43.69%) - LeetSync

--- 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.cpp
+++ 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -11,37 +11,28 @@
  */
 class Solution {
 public:
-    TreeNode* build(vector<int>& inorder, int instart, int inend, vector<int>& postorder, int poststart, int postend, unordered_map<int, int>& inmap) {
-        // Base case: invalid range
-        if (instart > inend || poststart > postend)
-            return nullptr;
+    TreeNode* build(vector<int>& inorder,int instart,int inend,vector<int>& postorder,int poststart,int postend,unordered_map<int, int>& inmap)
+    {
+        if(instart>inend || poststart>postend)
+        return NULL;
 
-        // The root is the last element of the current postorder range
-        TreeNode* root = new TreeNode(postorder[postend]);
-
-        // Find the root's position in the inorder traversal
-        int inroot = inmap[root->val];
-        int inleft = inroot - instart;
-
-        // Recursively build left and right subtrees
-        root->left = build(inorder, instart, inroot - 1, postorder, poststart, poststart + inleft - 1, inmap);
-        root->right = build(inorder, inroot + 1, inend, postorder, poststart + inleft, postend - 1, inmap);
-
+        TreeNode* root=new TreeNode(postorder[postend]);
+        int inroot=inmap[root->val];
+        int inleft=inroot-instart;
+        root->left=build(inorder, instart, inroot - 1,postorder,poststart,poststart+inleft-1, inmap);
+        root->right=build(inorder, inroot + 1, inend,postorder,poststart+inleft ,postend-1, inmap);
         return root;
-    }
+    } 
 
     TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
-        // If sizes do not match, return null
-        if (inorder.size() != postorder.size())
+        if (postorder.size() != inorder.size())
             return nullptr;
-
-        // Create a hashmap for quick index lookup
+        int size=inorder.size();
         unordered_map<int, int> inmap;
         for (int i = 0; i < inorder.size(); i++) {
             inmap[inorder[i]] = i;
         }
 
-        // Build the tree
-        return build(inorder, 0, inorder.size() - 1, postorder, 0, postorder.size() - 1, inmap);
+        return build(inorder,0,size-1,postorder,0,size-1,inmap);
     }
 };
