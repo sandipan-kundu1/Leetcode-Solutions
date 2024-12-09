f926bf869d11bd290e95fafdbe8e4bc2c17d1ba4
Sandipan Kundu <158579989+sandipan-kundu1@users.noreply.github.com>
2024-12-10 02:50:29 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (5.57%) - LeetSync

--- 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.cpp
+++ 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.cpp
@@ -11,21 +11,23 @@
  */
 class Solution {
 public:
-    void flat(TreeNode* root,TreeNode* &prev)
-    {
-        if(root==NULL)
-        return;
-        flat(root->right,prev);
-        flat(root->left,prev);
-        root->right=prev;
-        root->left=NULL;
-        prev=root;
-    }
     void flatten(TreeNode* root) {
         if(root==NULL)
         return;
-        TreeNode* prev=NULL;
-        flat(root,prev);
+        stack<TreeNode*> st;
+        st.push(root);
+        while(!st.empty())
+        {
+            TreeNode* temp=st.top();
+            st.pop();
+            if(temp->right)
+            st.push(temp->right);
+            if(temp->left)
+            st.push(temp->left);
+            if(!st.empty())
+            temp->right=st.top();
+            temp->left=NULL;
+        }
 
     }
 };
