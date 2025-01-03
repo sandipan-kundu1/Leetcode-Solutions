e37be6c10424cada0fe95cf70da7ae96217a3e4b
Sandipan Kundu <158579989+sandipan-kundu1@users.noreply.github.com>
2025-01-03 11:08:01 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (50.28%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -3,23 +3,25 @@ public:
     int uniquePaths(int m, int n) {
         //topdown approach will be from [m-1][n-1] to [0][0] so moves up and left
         
-        vector<vector<int>> dp(m,vector<int>(n,-1));
+        vector<int> prev(n);
         for(int i=0;i<m;i++)
         {
+            vector<int> cur(n);
             for(int j=0;j<n;j++)
             {
                 if(i==0 && j==0)
-                dp[i][j]=1;
+                cur[j]=1;
                 else
                 {
                     int up=0,left=0;
-                    if(i>0) up=dp[i-1][j];
-                    if(j>0) left=dp[i][j-1];
-                    dp[i][j]=up+left;
+                    if(i>0) up=prev[j];
+                    if(j>0) left=cur[j-1];
+                    cur[j]=up+left;
                 }
                 
             }
+            prev=cur;
         }
-        return dp[m-1][n-1];
+        return prev[n-1];
     }
 };
