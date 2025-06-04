51301c64dcce3ccf6c8dd6f7b7a9e23b725faabf
Sandipan Kundu <158579989+sandipan-kundu1@users.noreply.github.com>
2025-06-05 02:15:15 +0530

    Time: 0 ms (100.00%) | Memory: 10.3 MB (8.74%) - LeetSync

--- 48-rotate-image/rotate-image.cpp
+++ 48-rotate-image/rotate-image.cpp
@@ -1,24 +1,14 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
+        vector<int> order;
         int n=matrix.size();
-        for(int i=0;i<n-1;i++){
-            for(int j=i+1;j<n;j++)     //TC: O(n)+O(n^2)     SC:O(1)
-            swap(matrix[i][j],matrix[j][i]);
-        }
-        for(int i=0;i<n;i++){
-            reverse(matrix[i].begin(),matrix[i].end());
-        }
-        // vector<vector<int>> ans(n, vector<int>(n));
-        // for(int i=0;i<n;i++){
-        //     for(int j=0;j<n;j++){
-        //         ans[j][n-1-i]=matrix[i][j];     //TC: O(n^2)    SC:O(n^2)
-        //     }
-        // }
-        // for(int i=0;i<n;i++){
-        //     for(int j=0;j<n;j++){
-        //         matrix[i][j]=ans[i][j];
-        //     }
-        // }
+        for(int col=0;col<n;col++)
+            for(int row=n-1;row>=0;row--)
+                order.push_back(matrix[row][col]);
+        int k=0;    
+        for(int i=0;i<n;i++)
+            for(int j=0;j<n;j++)
+                matrix[i][j]=order[k++];
     }
 };
