4dd8034fb960e861d6a4d8ab8b3a245679905c4b
Sandipan Kundu <158579989+sandipan-kundu1@users.noreply.github.com>
2025-06-06 03:03:19 +0530

    Time: 46 ms (36.51%) | Memory: 49.1 MB (32.45%) - LeetSync

---
+++ 322-coin-change/coin-change.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int f(int i,vector<int>& coins, int tar,vector<vector<int>>& dp)
+    {
+        if(i==0)
+        {
+            if(tar%coins[0]==0)
+            return tar/coins[0];
+            return 1e9;//impossible to make coin combination
+        }
+        
+
+        if(dp[i][tar]!=-1)
+        return dp[i][tar];
+
+        int take=INT_MAX;
+        if(tar>=coins[i])
+        take=1+f(i,coins,tar-coins[i],dp);
+        int nottake=f(i-1,coins,tar,dp);
+        return dp[i][tar]=min(take,nottake);
+    }
+    int coinChange(vector<int>& coins, int amount) {
+        if(amount==0)
+        return 0;
+        int n=coins.size();
+        vector<vector<int>> dp(n,vector<int>(amount+1,-1));
+        if(f(coins.size()-1,coins,amount,dp)!=1e9)
+        return f(coins.size()-1,coins,amount,dp);
+        return -1;
+    }
+};
