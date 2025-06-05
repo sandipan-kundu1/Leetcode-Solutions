c7ef80c5a84fd59c75d507b56c29c46069fc33a6
Sandipan Kundu <158579989+sandipan-kundu1@users.noreply.github.com>
2025-06-06 02:31:55 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (21.12%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -1,18 +1,31 @@
 class Solution {
 public:
-    int robhelp(vector<int>& nums,int start, int end) {
-        int n=nums.size();
+    
+    // int f(int ind,vector<int>& dp, vector<int>& nums)
+    // {
+    //     if(ind<0)
+    //     return 0;
+
+    //     if(dp[ind]!=-1)
+    //     return dp[ind];
+
+    //     int take=nums[ind]+f(ind-2,dp,nums);
+    //     int nottake=f(ind-1,dp,nums);
+    //     return max(take,nottake);
+    //}
+    int robhelp(int start,int end,vector<int> &nums)
+    {
         if(start==end)
         return nums[start];
-
-        vector<int> dp(n);
+        
+        vector<int> dp(nums.size(),-1);
         dp[start]=nums[start];
-        dp[start+1]=max(nums[start], nums[start+1]);
+        dp[start+1]=max(nums[start],nums[start+1]);
         for(int i=start+2;i<=end;i++)
         {
-            int pick=nums[i]+dp[i-2];
-            int notpick=0+dp[i-1];
-            dp[i]=max(pick, notpick);
+            int take=nums[i]+dp[i-2];
+            int nottake=dp[i-1];
+            dp[i]=max(take,nottake);
         }
         return dp[end];
     }
@@ -20,8 +33,9 @@ public:
         int n=nums.size();
         if(n==1)
         return nums[0];
-        int first=robhelp(nums,0,n-2);
-        int last=robhelp(nums,1,n-1);
+
+        int first=robhelp(0,n-2,nums);
+        int last=robhelp(1,n-1,nums);
         return max(first,last);
     }
 };
