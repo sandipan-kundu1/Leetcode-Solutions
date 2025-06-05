9338db3feec31c064cebfc442d6e4e9a6992bb73
Sandipan Kundu <158579989+sandipan-kundu1@users.noreply.github.com>
2025-06-06 01:40:25 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (87.28%) - LeetSync

--- 190-reverse-bits/reverse-bits.cpp
+++ 190-reverse-bits/reverse-bits.cpp
@@ -3,9 +3,8 @@ public:
     uint32_t reverseBits(uint32_t n) {
         uint32_t ans=0;
         for(int i=0;i<32;i++) {
-            ans<<=1;
-            ans|=(n&1);
-            n>>=1;
+            int bit=(n>>i) & 1; //storing ith bit
+            ans=ans | bit<<(31-i);//adding it to its reverse position by 31-i
         }
         return ans;
     }
