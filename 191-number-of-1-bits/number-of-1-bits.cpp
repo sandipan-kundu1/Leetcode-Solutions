1f74180864da3e2b86b0739f623744b2964ef957
Sandipan Kundu <158579989+sandipan-kundu1@users.noreply.github.com>
2025-06-05 22:48:52 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (80.67%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.cpp
+++ 191-number-of-1-bits/number-of-1-bits.cpp
@@ -1,6 +1,11 @@
 class Solution {
 public:
     int hammingWeight(int n) {
-        return __builtin_popcount(n);
+        int cnt=0;
+        while(n){
+            n=n& (n-1);
+            cnt++; //clearing rightmost bit in each iteration, counting the iterations to get no of set bits
+        }
+        return cnt;
     }
 };
