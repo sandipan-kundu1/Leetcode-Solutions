1121e318796393e3ed6e57f19589627e132d138a
Sandipan Kundu <158579989+sandipan-kundu1@users.noreply.github.com>
2025-06-14 01:39:02 +0530

    Time: 87 ms (42.05%) | Memory: 174.2 MB (15.97%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,37 +1,26 @@
 class Solution {
 public:
-    string helper(int l,int r,string s)
-    {
-        int n=s.size();
-        string res="";
-        int reslen=0;
-        while(l>=0 && r<n && s[l]==s[r])
-            {
-                if(r-l+1>reslen)
-                {
-                    reslen=r-l+1;
-                    res=s.substr(l,r-l+1); //substr(starting index, length of substring)
-                }
-                l--;
-                r++;
-            }
-        
-        
-        return res;
+    string helper(int l, int r, string s) {
+        int n = s.size();
+        while (l >= 0 && r < n && s[l] == s[r]) {
+            l--;
+            r++;
+        }
+        // When the loop breaks, l and r have gone 1 step too far
+        return s.substr(l + 1, r - l - 1);
     }
+
     string longestPalindrome(string s) {
-        int n=s.size();
-        string res="";
-        for(int i=0;i<n;i++)
-        {
-            string odd=helper(i,i,s);
-            if(res.size()<odd.size())
-            res=odd;
-            string even=helper(i,i+1,s);
-            if(res.size()<even.size())
-            res=even;
+        string res = "";
+        for (int i = 0; i < s.size(); i++) {
+            // Odd length
+            string odd = helper(i, i, s);
+            if (odd.size() > res.size()) res = odd;
+
+            // Even length
+            string even = helper(i, i + 1, s);
+            if (even.size() > res.size()) res = even;
         }
         return res;
-
     }
 };
