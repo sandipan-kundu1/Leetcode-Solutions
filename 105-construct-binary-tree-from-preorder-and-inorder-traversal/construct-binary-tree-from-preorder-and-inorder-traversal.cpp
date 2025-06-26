2809c86125c87ac3823e355f40e609fc9280044f
Sandipan Kundu <158579989+sandipan-kundu1@users.noreply.github.com>
2025-06-26 15:46:11 +0530

    Time: 0 ms (100.00%) | Memory: 27.6 MB (36.72%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -11,33 +11,27 @@
  */
 class Solution {
 public:
-    TreeNode* build(vector<int>& preorder, int prestart, int preend, vector<int>& inorder, int instart, int inend, unordered_map<int, int>& inmap) {
-        if (prestart > preend || instart > inend)
-            return nullptr;
+    unordered_map<int,int> hash;
+    TreeNode* build(int instart,int inend,int prestart,int preend,vector<int>& preorder, vector<int>& inorder)
+    {
+        if(instart>inend || prestart>preend)
+        return NULL;
 
-        // Create the root node
-        TreeNode* root = new TreeNode(preorder[prestart]);
+        TreeNode* root=new TreeNode(preorder[prestart]);
+        int idx=hash[root->val];    //root's index in inorder
+        int inleft=idx-instart;
 
-        // Find the index of the root in inorder traversal
-        int inroot = inmap[root->val];
-        int inleft = inroot - instart;
-
-        // Build left and right subtrees
-        root->left = build(preorder, prestart + 1, prestart + inleft, inorder, instart, inroot - 1, inmap);
-        root->right = build(preorder, prestart + inleft + 1, preend, inorder, inroot + 1, inend, inmap);
+        root->left=build(instart,idx-1,prestart+1,prestart+inleft,preorder,inorder);
+        root->right=build(idx+1,inend,prestart+inleft+1,preend,preorder,inorder);
 
         return root;
     }
-
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        if (preorder.size() != inorder.size())
-            return nullptr;
-
-        unordered_map<int, int> inmap;
-        for (int i = 0; i < inorder.size(); i++) {
-            inmap[inorder[i]] = i;
+        int n=inorder.size();
+        for(int i=0;i<n;i++)
+        {
+            hash[inorder[i]]=i;
         }
-
-        return build(preorder, 0, preorder.size() - 1, inorder, 0, inorder.size() - 1, inmap);
+        return build(0,n-1,0,n-1,preorder,inorder);
     }
 };
