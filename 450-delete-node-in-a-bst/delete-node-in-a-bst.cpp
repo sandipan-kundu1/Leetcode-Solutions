7e5e991be0f22cdca9a9572262a1bcfa75e882f6
Sandipan Kundu <158579989+sandipan-kundu1@users.noreply.github.com>
2025-06-27 17:36:57 +0530

    Time: 0 ms (100.00%) | Memory: 34.4 MB (29.07%) - LeetSync

--- 450-delete-node-in-a-bst/delete-node-in-a-bst.cpp
+++ 450-delete-node-in-a-bst/delete-node-in-a-bst.cpp
@@ -6,51 +6,57 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
     TreeNode* helper(TreeNode* root)
     {
-        if(root->left==NULL)
+        if(!root->right)
+        return root->left;
+        if(!root->left)
         return root->right;
-        if(root->right==NULL)
-        return root->left;
 
-        TreeNode* rightchild=root->right;
-        TreeNode* cur=root->left;
-        while(cur->right!=NULL)
-        {
-            cur=cur->right;
-        }
-        cur->right=rightchild;
-        return root->left;
+        TreeNode* leftChild=root->left;
+        auto leftmost=root->right;
+
+        while(leftmost->left)
+        leftmost=leftmost->left;//finding leftmost node of right subtree
+
+        leftmost->left=leftChild;
+        return root->right;
     }
     TreeNode* deleteNode(TreeNode* root, int key) {
-        if (root == NULL)
-            return NULL;
-        TreeNode* dummy = root;
-        if (key == root->val) {
-            return helper(root);
-        }
-        while (root) {
+        if(!root)
+        return NULL;
 
-            if (key < root->val)
+        if(root->val==key)
+        return helper(root);
+        TreeNode* temp=root;
+        while(temp)
+        {
+            if(temp->val<key)
             {
-                if(root->left!=NULL && root->left->val==key)
-                root->left=helper(root->left);
+                if(temp->right && temp->right->val==key)
+                {
+                    temp->right=helper(temp->right);
+                    break;
+                }
                 else
-                root = root->left;
-            } 
-            else{
-                if(root->right!=NULL && root->right->val==key)
-                root->right=helper(root->right);
+                temp=temp->right;
+            }
+            else if(temp->val>key)
+            {
+                if(temp->left && temp->left->val==key)
+                {
+                    temp->left=helper(temp->left);
+                    break;
+                }
                 else
-                root = root->right;
-            } 
+                temp=temp->left;
+            }
         }
-        return dummy;
+        return root;
     }
 };
