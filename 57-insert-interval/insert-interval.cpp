3094912aa2f2051c9437e130baa6b997e51fb8ae
Sandipan Kundu <158579989+sandipan-kundu1@users.noreply.github.com>
2025-07-01 01:58:35 +0530

    Time: 0 ms (100.00%) | Memory: 21.6 MB (74.65%) - LeetSync

--- 57-insert-interval/insert-interval.cpp
+++ 57-insert-interval/insert-interval.cpp
@@ -1,30 +1,28 @@
 class Solution {
 public:
-    vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        vector<vector<int>> merged;
-        sort(intervals.begin(),intervals.end(), [](vector<int>&a,vector<int>&b){
-            return a[0]<b[0];
-        });
-
-        int time=-1,prevstart=-1;
-        for(auto &p:intervals)
-        {
-            int start=p[0],end=p[1];
-
-            if(start>time)
-            {
-                if(prevstart!=-1)
-                merged.push_back({prevstart,time});
-                prevstart=start;
-            }
-            time=max(time,end);
-        }
-        merged.push_back({prevstart,time});
-        return merged;
-    }
-
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-        intervals.push_back(newInterval);
-        return merge(intervals);
+        int newstart=newInterval[0],newend=newInterval[1];
+        int i=0,n=intervals.size();
+        vector<vector<int>> res;
+        while(i<n && intervals[i][1]<newstart)
+        {
+            res.push_back(intervals[i]);
+            i++;
+        }
+        //main position of insertion
+        while(i<n && newInterval[1]>=intervals[i][0])
+        {
+            newstart=min(newstart,intervals[i][0]);
+            newend=max(newend,intervals[i][1]);
+            i++;
+        }
+        res.push_back({newstart,newend});
+
+        while(i<n)
+        {
+            res.push_back(intervals[i]);
+            i++;
+        }
+        return res;
     }
 };
