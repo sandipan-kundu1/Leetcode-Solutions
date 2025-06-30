8ab882771832a3e7d4d6f52172ac3f5d6920aa44
Sandipan Kundu <158579989+sandipan-kundu1@users.noreply.github.com>
2025-07-01 03:53:49 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (46.38%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.cpp
+++ 678-valid-parenthesis-string/valid-parenthesis-string.cpp
@@ -1,23 +1,29 @@
 class Solution {
 public:
-    bool f(int i,int cnt,string s,vector<vector<int>>& dp)
-    {
-        if(i==s.size())
-        return cnt==0;
-        if(cnt<0)
-        return false;
-
-        if(dp[i][cnt]!=-1)
-        return (bool)dp[i][cnt];
-        if(s[i]=='(')
-        return dp[i][cnt]= (int)f(i+1,cnt+1,s,dp);
-        if(s[i]==')')
-        return dp[i][cnt]=(int) f(i+1,cnt-1,s,dp);
-
-        return dp[i][cnt]=(int)( f(i+1,cnt+1,s,dp) || f(i+1,cnt,s,dp) || f(i+1,cnt-1,s,dp) );
-    }
     bool checkValidString(string s) {
-        vector<vector<int>> dp(s.size(),vector<int>(s.size(),-1));
-        return f(0,0,s,dp);
+        int mini=0,maxi=0;
+        for(auto c:s)
+        {
+            if(c=='(')
+            {
+                mini++;
+                maxi++;
+            }
+            else if(c==')')
+            {
+                mini--;
+                maxi--;
+            }
+            else
+            {
+                mini--;
+                maxi++;
+            }
+            if(mini<0)
+            mini=0;
+            if(maxi<0)
+            return false;
+        }
+        return mini==0;
     }
 };
