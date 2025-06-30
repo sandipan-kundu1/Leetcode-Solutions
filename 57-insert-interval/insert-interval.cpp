5fbd8db9e1db9abe2dc551d7dd65880166051852
Sandipan Kundu <158579989+sandipan-kundu1@users.noreply.github.com>
2025-07-01 01:41:15 +0530

    Time: 5 ms (11.67%) | Memory: 21.6 MB (74.65%) - LeetSync

--- 57-insert-interval/insert-interval.cpp
+++ 57-insert-interval/insert-interval.cpp
@@ -1,30 +1,30 @@
 class Solution {
 public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        vector<vector<int>> merged;
+        sort(intervals.begin(),intervals.end(), [](vector<int>&a,vector<int>&b){
+            return a[0]<b[0];
+        });
+
+        int time=-1,prevstart=-1;
+        for(auto &p:intervals)
+        {
+            int start=p[0],end=p[1];
+
+            if(start>time)
+            {
+                if(prevstart!=-1)
+                merged.push_back({prevstart,time});
+                prevstart=start;
+            }
+            time=max(time,end);
+        }
+        merged.push_back({prevstart,time});
+        return merged;
+    }
+
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-        vector<vector<int>> ans;
-        int i = 0;
-        int n = intervals.size();
-
-        // Add all intervals ending before newInterval starts
-        while (i < n && intervals[i][1] < newInterval[0]) {
-            ans.push_back(intervals[i]);
-            i++;
-        }
-
-        // Merge all overlapping intervals to one considering newInterval
-        while (i < n && intervals[i][0] <= newInterval[1]) {
-            newInterval[0] = min(intervals[i][0], newInterval[0]);
-            newInterval[1] = max(intervals[i][1], newInterval[1]);
-            i++;
-        }
-        ans.push_back(newInterval); // Add the merged interval
-
-        // Add all the remaining intervals
-        while (i < n) {
-            ans.push_back(intervals[i]);
-            i++;
-        }
-
-        return ans;
+        intervals.push_back(newInterval);
+        return merge(intervals);
     }
 };
