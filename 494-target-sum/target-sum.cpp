b00a522e47f2301d639566e2a7973d0c9d47fa6c
Sandipan Kundu <158579989+sandipan-kundu1@users.noreply.github.com>
2025-07-04 01:05:28 +0530

    Time: 7 ms (70.05%) | Memory: 11.8 MB (75.65%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,19 +1,26 @@
 class Solution {
 public:
     int key;
+    int total;
+    int dp[21][2001];
     int f(int i,int sum,vector<int>& nums)
     {
         if(i<0)
         {
             return sum==key;
         }
+        if(dp[i][sum+total]!=-1)
+        return dp[i][sum+total];
+        
         int add=f(i-1,sum+nums[i],nums);
         int sub=f(i-1,sum-nums[i],nums);
-        return add+sub;
+        return dp[i][sum+total]= add+sub;
     }
     int findTargetSumWays(vector<int>& nums, int target) {
         key=target;
         int n=nums.size();
+        total=accumulate(nums.begin(),nums.end(),0);
+        memset(dp,-1,sizeof(dp));
         return f(n-1,0,nums);
     }
 };
