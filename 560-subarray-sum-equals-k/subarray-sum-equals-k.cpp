534ce7fd2722f1c3371f8d1950ff6e17633ffdee
Sandipan Kundu <158579989+sandipan-kundu1@users.noreply.github.com>
2025-10-05 21:13:22 +0530

    Time: 49 ms (51.78%) | Memory: 55.4 MB (7.10%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        int n = nums.size(), cnt = 0, sum = 0;
-        unordered_map<int, int> hash;
-        hash[0] = 1; // There is one way to have a sum of 0, which is having no elements.
+        //x-k,k method prefix sum map
+        unordered_map<long long,int> prefix;
+        prefix[0]=1;
+        int n=nums.size(),cnt=0;
+        long long sum=0;
+        for(int i=0;i<n;i++)
+        {
+            sum+=nums[i];
+            long long rem=sum-k;
+            if(prefix[rem])
+            cnt+=prefix[rem];
 
-        for (int i = 0; i < n; ++i) {
-            sum += nums[i];
-            if (hash.find(sum - k) != hash.end()) {
-                cnt += hash[sum - k];
-            }
-            hash[sum]++;
+            prefix[sum]++;
         }
         return cnt;
     }
